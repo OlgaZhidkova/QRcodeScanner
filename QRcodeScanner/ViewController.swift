4221e7bcf15380031b1bfca797598d1b0734a6b1
Olga Zhidkova <zhidkova.olga12@gmail.com>
2022-04-29 18:12:17 +0300

    Make qr scanner.

--- QRcodeScanner/ViewController.swift
+++ QRcodeScanner/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftQRScanner
 
 class ViewController: UIViewController {
 
@@ -14,6 +15,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func scanQRAction(_ sender: Any) {
+        //QRCode scanner without Camera switch and Torch
+        let scanner = QRCodeScannerController()
+        
+        //QRCode with Camera switch and Torch
+        //let scanner = QRCodeScannerController(cameraImage: UIImage(named: "camera"), cancelImage: UIImage(named: "cancel"), flashOnImage: UIImage(named: "flash-on"), flashOffImage: UIImage(named: "flash-off"))
+        scanner.delegate = self
+        self.present(scanner, animated: true, completion: nil)
+    }
+    
 }
 
+extension ViewController: QRScannerCodeDelegate {
+    func qrScanner(_ controller: UIViewController, scanDidComplete result: String) {
+        print("result:\(result)")
+        if let url = URL(string: result) {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    func qrScannerDidFail(_ controller: UIViewController, error: String) {
+        print("error:\(error)")
+    }
+    
+    func qrScannerDidCancel(_ controller: UIViewController) {
+        print("SwiftQRScanner did cancel")
+    }
+}
